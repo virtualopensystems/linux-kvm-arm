bc8dcc2107de0ba8f25fc910c4559ebe3df33045
Avi Kivity <avi@qumranet.com>
2007-05-02 16:54:03 +0300

    KVM: Fix potential guest state leak into host
    The lightweight vmexit path avoids saving and reloading certain host
    state.  However in certain cases lightweight vmexit handling can schedule()
    which requires reloading the host state.
    
    So we store the host state in the vcpu structure, and reloaded it if we
    relinquish the vcpu.
    
    Signed-off-by: Avi Kivity <avi@qumranet.com>

--- drivers/kvm/kvm.h
+++ drivers/kvm/kvm.h
@@ -304,6 +304,11 @@ struct kvm_vcpu {
 	char *host_fx_image;
 	char *guest_fx_image;
 	int fpu_active;
+	struct vmx_host_state {
+		int loaded;
+		u16 fs_sel, gs_sel, ldt_sel;
+		int fs_gs_ldt_reload_needed;
+	} vmx_host_state;
 
 	int mmio_needed;
 	int mmio_read_completed;
