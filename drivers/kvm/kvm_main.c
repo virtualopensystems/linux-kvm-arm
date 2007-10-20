933a3f772d28b532cd09098257e482694a795dfb
Zhang Xiantao <xiantao.zhang@intel.com>
2007-10-20 15:34:38 +0800

    KVM: Portability: Split kvm_vcpu into arch dependent and independent parts (part 1)
    First step to split kvm_vcpu.  Currently, we just use an macro to define
    the common fields in kvm_vcpu for all archs, and all archs need to define
    its own kvm_vcpu struct.
    
    Signed-off-by: Zhang Xiantao <xiantao.zhang@intel.com>
    Signed-off-by: Avi Kivity <avi@qumranet.com>

--- drivers/kvm/kvm_main.c
+++ drivers/kvm/kvm_main.c
@@ -2274,7 +2274,7 @@ static int kvm_vcpu_ioctl_run(struct kvm_vcpu *vcpu, struct kvm_run *kvm_run)
 		if (r)
 			goto out;
 	}
-
+#if CONFIG_HAS_IOMEM
 	if (vcpu->mmio_needed) {
 		memcpy(vcpu->mmio_data, kvm_run->mmio.data, 8);
 		vcpu->mmio_read_completed = 1;
@@ -2289,7 +2289,7 @@ static int kvm_vcpu_ioctl_run(struct kvm_vcpu *vcpu, struct kvm_run *kvm_run)
 			goto out;
 		}
 	}
-
+#endif
 	if (kvm_run->exit_reason == KVM_EXIT_HYPERCALL) {
 		kvm_x86_ops->cache_regs(vcpu);
 		vcpu->regs[VCPU_REGS_RAX] = kvm_run->hypercall.ret;
