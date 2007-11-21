1a4c6de93b1c39030475a61d02e4df7f74f51e37
Avi Kivity <avi@qumranet.com>
2007-11-21 16:41:05 +0200

    KVM: Disallow fork() and similar games when using a VM
    We don't want the meaning of guest userspace changing under our feet.
    
    Signed-off-by: Avi Kivity <avi@qumranet.com>

--- kernel/fork.c
+++ kernel/fork.c
@@ -392,6 +392,7 @@ void fastcall __mmdrop(struct mm_struct *mm)
 	destroy_context(mm);
 	free_mm(mm);
 }
+EXPORT_SYMBOL_GPL(__mmdrop);
 
 /*
  * Decrement the use count and release all resources for an mm.
