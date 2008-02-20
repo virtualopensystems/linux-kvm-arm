d6f633e2fc1931de330962c2c0f1bcf8036c5f8a
Avi Kivity <avi@qumranet.com>
2008-02-20 14:55:05 +0200

    KVM: VMX: Handle machines without EFER
    Signed-off-by: Avi Kivity <avi@qumranet.com>

--- arch/x86/kvm/vmx.c
+++ arch/x86/kvm/vmx.c
@@ -1342,6 +1342,8 @@ static void vmx_set_efer(struct kvm_vcpu *vcpu, u64 efer)
 	struct kvm_msr_entry *msr = find_msr_entry(vmx, MSR_EFER);
 
 	vcpu->arch.shadow_efer = efer;
+	if (!msr)
+		return;
 	if (efer & EFER_LMA) {
 		vmcs_write32(VM_ENTRY_CONTROLS,
 				     vmcs_read32(VM_ENTRY_CONTROLS) |
