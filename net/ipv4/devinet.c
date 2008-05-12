da0ea229b68554b1f0ba8591e5819daf240f07da
Robert Love <rlove@google.com>
2008-05-12 17:08:29 -0400

    net: socket ioctl to reset connections matching local address
    Introduce a new socket ioctl, SIOCKILLADDR, that nukes all sockets
    bound to the same local address. This is useful in situations with
    dynamic IPs, to kill stuck connections.
    
    Signed-off-by: Brian Swetland <swetland@google.com>
    
    net: fix tcp_v4_nuke_addr
    
    Signed-off-by: Dima Zavin <dima@android.com>
    
    net: ipv4: Fix a spinlock recursion bug in tcp_v4_nuke.
    
    We can't hold the lock while calling to tcp_done(), so we drop
    it before calling. We then have to start at the top of the chain again.
    
    Signed-off-by: Dima Zavin <dima@android.com>
    
    net: ipv4: Fix race in tcp_v4_nuke_addr().
    
    To fix a recursive deadlock in 2.6.29, we stopped holding the hash table lock
    across tcp_done() calls. This fixed the deadlock, but introduced a race where
    the socket could die or change state.
    
    Fix: Before unlocking the hash table, we grab a reference to the socket. We
    can then unlock the hash table without risk of the socket going away. We then
    lock the socket, which is safe because it is pinned. We can then call
    tcp_done() without recursive deadlock and without race. Upon return, we unlock
    the socket and then unpin it, killing it.
    
    Change-Id: Idcdae072b48238b01bdbc8823b60310f1976e045
    Signed-off-by: Robert Love <rlove@google.com>
    Acked-by: Dima Zavin <dima@android.com>
    
    ipv4: disable bottom halves around call to tcp_done().
    
    Signed-off-by: Robert Love <rlove@google.com>
    Signed-off-by: Colin Cross <ccross@android.com>
    
    ipv4: Move sk_error_report inside bh_lock_sock in tcp_v4_nuke_addr
    
    When sk_error_report is called, it wakes up the user-space thread, which then
    calls tcp_close.  When the tcp_close is interrupted by the tcp_v4_nuke_addr
    ioctl thread running tcp_done, it leaks 392 bytes and triggers a WARN_ON.
    
    This patch moves the call to sk_error_report inside the bh_lock_sock, which
    matches the locking used in tcp_v4_err.
    
    Signed-off-by: Colin Cross <ccross@android.com>

--- net/ipv4/devinet.c
+++ net/ipv4/devinet.c
@@ -58,6 +58,7 @@
 
 #include <net/arp.h>
 #include <net/ip.h>
+#include <net/tcp.h>
 #include <net/route.h>
 #include <net/ip_fib.h>
 #include <net/rtnetlink.h>
@@ -730,6 +731,7 @@ int devinet_ioctl(struct net *net, unsigned int cmd, void __user *arg)
 	case SIOCSIFBRDADDR:	/* Set the broadcast address */
 	case SIOCSIFDSTADDR:	/* Set the destination address */
 	case SIOCSIFNETMASK: 	/* Set the netmask for the interface */
+	case SIOCKILLADDR:	/* Nuke all sockets on this address */
 		ret = -EPERM;
 		if (!capable(CAP_NET_ADMIN))
 			goto out;
@@ -781,7 +783,8 @@ int devinet_ioctl(struct net *net, unsigned int cmd, void __user *arg)
 	}
 
 	ret = -EADDRNOTAVAIL;
-	if (!ifa && cmd != SIOCSIFADDR && cmd != SIOCSIFFLAGS)
+	if (!ifa && cmd != SIOCSIFADDR && cmd != SIOCSIFFLAGS
+	    && cmd != SIOCKILLADDR)
 		goto done;
 
 	switch (cmd) {
@@ -906,6 +909,9 @@ int devinet_ioctl(struct net *net, unsigned int cmd, void __user *arg)
 			}
 			inet_insert_ifa(ifa);
 		}
+		break;
+	case SIOCKILLADDR:	/* Nuke all connections on this address */
+		ret = tcp_nuke_addr(net, (struct sockaddr *) sin);
 		break;
 	}
 done:
