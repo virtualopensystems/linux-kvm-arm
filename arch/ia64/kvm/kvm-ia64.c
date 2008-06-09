fecf859b27a250d7dc622346a7e357b387534566
Andrew Morton <akpm@linux-foundation.org>
2008-06-09 04:01:11 -0700

    KVM: ia64: fix compile error in coalesced mmio
    Cc: "Zhang, Xiantao" <xiantao.zhang@intel.com>
    Cc: "Avi Kivity" <avi@qumranet.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Avi Kivity <avi@qumranet.com>

--- arch/ia64/kvm/kvm-ia64.c
+++ arch/ia64/kvm/kvm-ia64.c
@@ -187,7 +187,7 @@ int kvm_dev_ioctl_check_extension(long ext)
 
 		r = 1;
 		break;
-	case KVM_CAP_COALESCED_MMIO::
+	case KVM_CAP_COALESCED_MMIO:
 		r = KVM_COALESCED_MMIO_PAGE_OFFSET;
 		break;
 	default:
