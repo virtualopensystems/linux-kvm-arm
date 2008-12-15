270a538ccceef15f615d0d890638604c79b82423
Jan Kiszka <jan.kiszka@siemens.com>
2008-12-15 13:52:10 +0100

    KVM: VMX: Allow single-stepping when uninterruptible
    When single-stepping over STI and MOV SS, we must clear the
    corresponding interruptibility bits in the guest state. Otherwise
    vmentry fails as it then expects bit 14 (BS) in pending debug exceptions
    being set, but that's not correct for the guest debugging case.
    
    Note that clearing those bits is safe as we check for interruptibility
    based on the original state and do not inject interrupts or NMIs if
    guest interruptibility was blocked.
    
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>
    Signed-off-by: Avi Kivity <avi@redhat.com>

--- arch/x86/kvm/vmx.c
+++ arch/x86/kvm/vmx.c
@@ -2477,6 +2477,11 @@ static void do_interrupt_requests(struct kvm_vcpu *vcpu,
 {
 	vmx_update_window_states(vcpu);
 
+	if (vcpu->guest_debug & KVM_GUESTDBG_SINGLESTEP)
+		vmcs_clear_bits(GUEST_INTERRUPTIBILITY_INFO,
+				GUEST_INTR_STATE_STI |
+				GUEST_INTR_STATE_MOV_SS);
+
 	if (vcpu->arch.nmi_pending && !vcpu->arch.nmi_injected) {
 		if (vcpu->arch.interrupt.pending) {
 			enable_nmi_window(vcpu);
@@ -3242,6 +3247,11 @@ static void vmx_intr_assist(struct kvm_vcpu *vcpu)
 	update_tpr_threshold(vcpu);
 
 	vmx_update_window_states(vcpu);
+
+	if (vcpu->guest_debug & KVM_GUESTDBG_SINGLESTEP)
+		vmcs_clear_bits(GUEST_INTERRUPTIBILITY_INFO,
+				GUEST_INTR_STATE_STI |
+				GUEST_INTR_STATE_MOV_SS);
 
 	if (vcpu->arch.nmi_pending && !vcpu->arch.nmi_injected) {
 		if (vcpu->arch.interrupt.pending) {
