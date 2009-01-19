5326eee2be7b747e71f29b9b6abeb7255b4e352a
Avi Kivity <avi@redhat.com>
2009-01-19 14:57:52 +0200

    KVM: Avoid using CONFIG_ in userspace visible headers
    Kconfig symbols are not available in userspace, and are not stripped by
    headers-install.  Avoid their use by adding #defines in <asm/kvm.h> to
    suit each architecture.
    
    Signed-off-by: Avi Kivity <avi@redhat.com>

--- arch/x86/include/asm/kvm.h
+++ arch/x86/include/asm/kvm.h
@@ -9,6 +9,14 @@
 #include <asm/types.h>
 #include <linux/ioctl.h>
 
+/* Select x86 specific features in <linux/kvm.h> */
+#define __KVM_HAVE_PIT
+#define __KVM_HAVE_IOAPIC
+#define __KVM_HAVE_DEVICE_ASSIGNMENT
+#define __KVM_HAVE_MSI
+#define __KVM_HAVE_USER_NMI
+#define __KVM_HAVE_GUEST_DEBUG
+
 /* Architectural interrupt line count. */
 #define KVM_NR_INTERRUPTS 256
 
