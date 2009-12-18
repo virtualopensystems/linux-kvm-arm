3e964e1ae21174196c3881559a0c10bb487485a3
Sheng Yang <sheng@linux.intel.com>
2009-12-18 16:48:46 +0800

    KVM: Add cpuid_update() callback to kvm_x86_ops
    Sometime, we need to adjust some state in order to reflect guest CPUID
    setting, e.g. if we don't expose rdtscp to guest, we won't want to enable
    it on hardware. cpuid_update() is introduced for this purpose.
    
    Also export kvm_find_cpuid_entry() for later use.
    
    Signed-off-by: Sheng Yang <sheng@linux.intel.com>
    Signed-off-by: Avi Kivity <avi@redhat.com>

--- arch/x86/kvm/vmx.c
+++ arch/x86/kvm/vmx.c
@@ -3987,6 +3987,10 @@ static bool vmx_gb_page_enable(void)
 	return false;
 }
 
+static void vmx_cpuid_update(struct kvm_vcpu *vcpu)
+{
+}
+
 static struct kvm_x86_ops vmx_x86_ops = {
 	.cpu_has_kvm_support = cpu_has_kvm_support,
 	.disabled_by_bios = vmx_disabled_by_bios,
@@ -4051,6 +4055,8 @@ static struct kvm_x86_ops vmx_x86_ops = {
 
 	.exit_reasons_str = vmx_exit_reasons_str,
 	.gb_page_enable = vmx_gb_page_enable,
+
+	.cpuid_update = vmx_cpuid_update,
 };
 
 static int __init vmx_init(void)
