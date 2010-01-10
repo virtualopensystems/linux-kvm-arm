e43f2f741a49483034bf968841275cfa553a4cb3
Alexander Graf <agraf@suse.de>
2010-01-10 18:01:48 +0100

    KVM: PPC: E500 compile fix
    While trying to compile an E500 vmlinux, I stumbled across a compilation bug
    that was obviously there before I touched any of the code. A trace point
    doesn't get the correct arguments.
    
    Since that shouldn't be any critical to the functionality of the code, my quick
    workaround is to #if 0 it out.
    
    Signed-off-by: Alexander Graf <agraf@suse.de>
    CC: Liu Yu <yu.liu@freescale.com>
    Signed-off-by: Avi Kivity <avi@redhat.com>

--- arch/powerpc/kvm/e500_tlb.c
+++ arch/powerpc/kvm/e500_tlb.c
@@ -225,8 +225,11 @@ static void kvmppc_e500_stlbe_invalidate(struct kvmppc_vcpu_e500 *vcpu_e500,
 
 	kvmppc_e500_shadow_release(vcpu_e500, tlbsel, esel);
 	stlbe->mas1 = 0;
+	/* XXX doesn't compile */
+#if 0
 	trace_kvm_stlb_inval(index_of(tlbsel, esel), stlbe->mas1, stlbe->mas2,
 			     stlbe->mas3, stlbe->mas7);
+#endif
 }
 
 static void kvmppc_e500_tlb1_invalidate(struct kvmppc_vcpu_e500 *vcpu_e500,
