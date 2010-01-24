6ff52ca8ec21e2fbe351f8ab699496ab6946e4aa
Avi Kivity <avi@redhat.com>
2010-01-24 12:17:23 +0200

    KVM: VMX: Fix clts
    The simplistic clts implementation has a couple of flaws:
    - kvm_read_cr0_bits() is temporarily unsynchronized when vcpu->arch.cr0 changes
    - if the fpu is active, we need to clear GUEST_CR0.TS, not just
      CR_READ_SHADOW.TS, so that we don't send the guest an unexpected #NM.
    
    Fix by replacing custom logic with a call to vmx_set_cr0(), which does the
    right thing, albeit less efficiently.
    
    Signed-off-by: Avi Kivity <avi@redhat.com>
    Signed-off-by: Marcelo Tosatti <mtosatti@redhat.com>

--- arch/x86/kvm/vmx.c
+++ arch/x86/kvm/vmx.c
@@ -3002,8 +3002,7 @@ static int handle_cr(struct kvm_vcpu *vcpu)
 		};
 		break;
 	case 2: /* clts */
-		vcpu->arch.cr0 &= ~X86_CR0_TS;
-		vmcs_writel(CR0_READ_SHADOW, kvm_read_cr0(vcpu));
+		vmx_set_cr0(vcpu, kvm_read_cr0_bits(vcpu, ~X86_CR0_TS));
 		trace_kvm_cr_write(0, kvm_read_cr0(vcpu));
 		skip_emulated_instruction(vcpu);
 		vmx_fpu_activate(vcpu);
