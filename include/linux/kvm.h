6b7859012c48e8feb39ffd665a30479a7f45382c
Alexander Graf <agraf@suse.de>
2010-02-19 11:00:45 +0100

    KVM: PPC: Add capability for paired singles
    We need to tell userspace that we can emulate paired single instructions.
    So let's add a capability export.
    
    Signed-off-by: Alexander Graf <agraf@suse.de>
    Signed-off-by: Avi Kivity <avi@redhat.com>

--- include/linux/kvm.h
+++ include/linux/kvm.h
@@ -501,6 +501,7 @@ struct kvm_ioeventfd {
 #define KVM_CAP_HYPERV_VAPIC 45
 #define KVM_CAP_HYPERV_SPIN 46
 #define KVM_CAP_PCI_SEGMENT 47
+#define KVM_CAP_PPC_PAIRED_SINGLES 48
 
 #ifdef KVM_CAP_IRQ_ROUTING
 
