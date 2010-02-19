a2606d54b10604c937aed9ba975919d71bee2da8
Joerg Roedel <joerg.roedel@amd.com>
2010-02-19 16:23:04 +0100

    KVM: SVM: Annotate nested_svm_map with might_sleep()
    The nested_svm_map() function can sleep and must not be
    called from atomic context. So annotate that function.
    
    Signed-off-by: Joerg Roedel <joerg.roedel@amd.com>
    Signed-off-by: Avi Kivity <avi@redhat.com>

--- arch/x86/kvm/svm.c
+++ arch/x86/kvm/svm.c
@@ -1428,6 +1428,8 @@ static void *nested_svm_map(struct vcpu_svm *svm, u64 gpa, struct page **_page)
 {
 	struct page *page;
 
+	might_sleep();
+
 	page = gfn_to_page(svm->vcpu.kvm, gpa >> PAGE_SHIFT);
 	if (is_error_page(page))
 		goto error;
