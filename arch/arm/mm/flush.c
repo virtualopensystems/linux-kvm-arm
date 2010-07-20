231ebe8d45c18ae557a24637d1b57acb29ea9f7a
Catalin Marinas <catalin.marinas@arm.com>
2010-07-20 14:23:32 +0100

    ARM: Allow lazy cache flushing via PG_arch_1 for highmem pages
    Current flush_dcache_page() implementation does not allow lazy cache
    flushing for highmem pages (introduced by commit d73cd42) on the
    assumption that the temporary kmap mapping would disappear. A subsequent
    commit (7e5a69e) allows __flush_dcache_page() to handle highmem pages so
    we can allow lazy cache flushing even for highmem pages.
    
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>
    Acked-by: Nicolas Pitre <nico@linaro.org>

--- arch/arm/mm/flush.c
+++ arch/arm/mm/flush.c
@@ -274,7 +274,7 @@ void flush_dcache_page(struct page *page)
 	mapping = page_mapping(page);
 
 	if (!cache_ops_need_broadcast() &&
-	    !PageHighMem(page) && mapping && !mapping_mapped(mapping))
+	    mapping && !mapping_mapped(mapping))
 		clear_bit(PG_dcache_clean, &page->flags);
 	else {
 		__flush_dcache_page(mapping, page);
