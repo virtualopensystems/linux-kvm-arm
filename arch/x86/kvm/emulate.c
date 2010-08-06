a2569690a0a02424ab0650b2a5e7a58dc53d56a0
Wei Yongjun <yjwei@cn.fujitsu.com>
2010-08-06 15:36:36 +0800

    KVM: x86 emulator: remove useless label from x86_emulate_insn()
    Signed-off-by: Wei Yongjun <yjwei@cn.fujitsu.com>
    Signed-off-by: Avi Kivity <avi@redhat.com>

--- arch/x86/kvm/emulate.c
+++ arch/x86/kvm/emulate.c
@@ -2788,16 +2788,12 @@ x86_emulate_insn(struct x86_emulate_ctxt *ctxt)
 		c->eip = ctxt->eip;
 	}
 
-	if (c->src.type == OP_MEM) {
-		if (c->d & NoAccess)
-			goto no_fetch;
+	if ((c->src.type == OP_MEM) && !(c->d & NoAccess)) {
 		rc = read_emulated(ctxt, ops, c->src.addr.mem,
 					c->src.valptr, c->src.bytes);
 		if (rc != X86EMUL_CONTINUE)
 			goto done;
 		c->src.orig_val = c->src.val;
-	no_fetch:
-		;
 	}
 
 	if (c->src2.type == OP_MEM) {
