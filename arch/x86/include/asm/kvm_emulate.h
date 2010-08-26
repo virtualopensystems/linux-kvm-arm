77a28e3547f8b041a6e940f5ec60296c091fd075
Avi Kivity <avi@redhat.com>
2010-08-26 14:31:30 +0300

    KVM: x86 emulator: fix regression with cmpxchg8b on i386 hosts
    operand::val and operand::orig_val are 32-bit on i386, whereas cmpxchg8b
    operands are 64-bit.
    
    Fix by adding val64 and orig_val64 union members to struct operand, and
    using them where needed.
    
    Signed-off-by: Avi Kivity <avi@redhat.com>
    Signed-off-by: Marcelo Tosatti <mtosatti@redhat.com>

--- arch/x86/include/asm/kvm_emulate.h
+++ arch/x86/include/asm/kvm_emulate.h
@@ -153,13 +153,17 @@ struct x86_emulate_ops {
 struct operand {
 	enum { OP_REG, OP_MEM, OP_IMM, OP_NONE } type;
 	unsigned int bytes;
-	unsigned long orig_val;
+	union {
+		unsigned long orig_val;
+		u64 orig_val64;
+	};
 	union {
 		unsigned long *reg;
 		unsigned long mem;
 	} addr;
 	union {
 		unsigned long val;
+		u64 val64;
 		char valptr[sizeof(unsigned long) + 2];
 	};
 };
