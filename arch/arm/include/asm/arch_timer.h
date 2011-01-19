2bbaa5b28a46f2b582c16d4aeab0b57a59ae3c3d
Marc Zyngier <marc.zyngier@arm.com>
2011-01-19 15:18:13 +0000

    ARM: architected timers: add A15 architected timers
    The ARM architecture provides a set of optional system timers that
    can be used as clocksources for local timer events.
    
    This patch adds support for these timers, giving them priority over
    any private timers if they are detected at runtime.
    
    A global counter is also used to implement a clock source.
    
    Acked-by: Will Deacon <will.deacon@arm.com>
    Acked-by: Catalin Marinas <catalin.marinas@arm.com>
    Signed-off-by: Marc Zyngier <marc.zyngier@arm.com>

---
+++ arch/arm/include/asm/arch_timer.h
@@ -0,0 +1,15 @@
+#ifndef __ASMARM_ARCH_TIMER_H
+#define __ASMARM_ARCH_TIMER_H
+
+#ifdef CONFIG_HAVE_ARCH_TIMERS
+int arch_timer_register_setup(int (*setup)(struct clock_event_device *),
+			      void (*teardown)(struct clock_event_device *));
+#else
+static inline int arch_timer_register_setup(int (*setup)(struct clock_event_device *),
+					    void (*teardown)(struct clock_event_device *))
+{
+	return -ENODEV;
+}
+#endif
+
+#endif
