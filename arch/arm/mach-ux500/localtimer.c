1752d376ba37ce2cfd4ede58f8e0ae1a2163ba04
Marc Zyngier <marc.zyngier@arm.com>
2011-01-26 17:31:32 +0000

    ARM: ux500: remove useless twd_timer_setup call
    Signed-off-by: Marc Zyngier <marc.zyngier@arm.com>

--- arch/arm/mach-ux500/localtimer.c
+++ arch/arm/mach-ux500/localtimer.c
@@ -11,11 +11,9 @@
  * published by the Free Software Foundation.
  */
 #include <linux/init.h>
-#include <linux/smp.h>
 #include <linux/clockchips.h>
 
 #include <asm/irq.h>
-#include <asm/smp_twd.h>
 #include <asm/localtimer.h>
 
 /*
@@ -24,5 +22,4 @@
 void __cpuinit local_timer_setup(struct clock_event_device *evt)
 {
 	evt->irq = IRQ_LOCALTIMER;
-	twd_timer_setup(evt);
 }
