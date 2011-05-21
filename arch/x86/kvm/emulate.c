e83a4c2971a9e71f75e5d54f1b5d057e3f1c9c04
Takuya Yoshikawa <yoshikawa.takuya@oss.ntt.co.jp>
2011-05-21 13:06:39 +0900

    KVM: x86 emulator: Fix unconditional return from get_descriptor_table_ptr()
    A trivial typo was found in the following commit:
      commit 7753ed6043bfce55dc0c407490896632014b677e
      KVM: x86 emulator: drop vcpu argument from segment/gdt/idt callbacks
    
    When the table indicator flag is set, when the selector selects the
    current LDT, get_descriptor_table_ptr() returns without setting the
    size and address of the table.
    
    Guests will see #GP if this happens.
    
    Signed-off-by: Takuya Yoshikawa <yoshikawa.takuya@oss.ntt.co.jp>
    Signed-off-by: Avi Kivity <avi@redhat.com>

--- arch/x86/kvm/emulate.c
+++ arch/x86/kvm/emulate.c
@@ -1158,7 +1158,7 @@ static void get_descriptor_table_ptr(struct x86_emulate_ctxt *ctxt,
 		u16 sel;
 
 		memset (dt, 0, sizeof *dt);
-		if (!ops->get_segment(ctxt, &sel, &desc, NULL, VCPU_SREG_LDTR));
+		if (!ops->get_segment(ctxt, &sel, &desc, NULL, VCPU_SREG_LDTR))
 			return;
 
 		dt->size = desc_limit_scaled(&desc); /* what if limit > 65535? */
