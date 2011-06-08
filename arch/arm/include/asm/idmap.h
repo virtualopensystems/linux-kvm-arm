0ea8c0ed34657b14d84caec3fba1adf0451bc619
Will Deacon <will.deacon@arm.com>
2011-06-08 15:53:34 +0100

    ARM: reset: allow kernelspace mappings to be flat mapped during reset
    Currently, switch_mm_for_reboot only takes out a 1:1 mapping from 0x0
    to TASK_SIZE during reboot. For situations where we actually want to
    turn off the MMU (e.g. kexec, hibernate, CPU hotplug) we want to map
    as much memory as possible using the identity mapping so that we
    increase the chance of mapping our reset code.
    
    This patch extends setup_mm_for_reboot to take a set of page tables as
    an optional parameter. If this is NULL then the behaviour is as before,
    otherwise the new tables are used to remap all of memory apart from a
    small window around the kernel image (TASK_SIZE - _end).
    
    Signed-off-by: Will Deacon <will.deacon@arm.com>

--- arch/arm/include/asm/idmap.h
+++ arch/arm/include/asm/idmap.h
@@ -6,6 +6,6 @@
 void identity_mapping_add(pgd_t *pgd, unsigned long addr, unsigned long end);
 void identity_mapping_del(pgd_t *pgd, unsigned long addr, unsigned long end);
 
-void setup_mm_for_reboot(char mode);
+void setup_mm_for_reboot(char mode, pgd_t *pgd);
 
 #endif	/* _ARM_IDMAP_H */
