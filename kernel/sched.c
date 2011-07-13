df42458172af9b510629cfd45c73425bc504826c
Avi Kivity <avi@redhat.com>
2011-07-13 16:22:19 +0300

    sched: conditionally include <asm/paravirt.h>
    Does not exist on many architectures.
    
    Signed-off-by: Avi Kivity <avi@redhat.com>

--- kernel/sched.c
+++ kernel/sched.c
@@ -75,7 +75,9 @@
 #include <asm/tlb.h>
 #include <asm/irq_regs.h>
 #include <asm/mutex.h>
+#ifdef CONFIG_PARAVIRT
 #include <asm/paravirt.h>
+#endif
 
 #include "sched_cpupri.h"
 #include "workqueue_sched.h"
