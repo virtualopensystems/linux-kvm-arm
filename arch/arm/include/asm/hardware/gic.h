01dd9d6ee478a892f94aa1d304674c5933c441bc
Marc Zyngier <marc.zyngier@arm.com>
2011-09-06 09:56:17 +0100

    ARM: GIC: Add global gic_handle_irq() function
    Provide the GIC code with a low level handler that can be used
    by platforms using CONFIG_MULTI_IRQ_HANDLER.
    
    Signed-off-by: Marc Zyngier <marc.zyngier@arm.com>

--- arch/arm/include/asm/hardware/gic.h
+++ arch/arm/include/asm/hardware/gic.h
@@ -42,6 +42,7 @@ extern struct irq_chip gic_arch_extn;
 void gic_init(unsigned int, int, void __iomem *, void __iomem *);
 int gic_of_init(struct device_node *node, struct device_node *parent);
 void gic_secondary_init_base(unsigned int, void __iomem *, void __iomem *);
+void gic_handle_irq(struct pt_regs *regs);
 void gic_cascade_irq(unsigned int gic_nr, unsigned int irq);
 void gic_raise_softirq(const struct cpumask *mask, unsigned int irq);
 
