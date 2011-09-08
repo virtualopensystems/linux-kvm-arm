2d5948cdfe0b79ba4bed2f37191ec52c39e96bdc
Marc Zyngier <marc.zyngier@arm.com>
2011-09-08 13:15:22 +0100

    ARM: SoC: convert VExpress/RealView to SoC descriptor
    Convert both Realview and VExpress to use the SoC descriptor to
    provide their SMP and CPU hotplug operation.
    
    Cc: Arnd Bergmann <arnd@arndb.de>
    Cc: Nicolas Pitre <nico@fluxnic.net>
    Signed-off-by: Marc Zyngier <marc.zyngier@arm.com>

--- arch/arm/mach-vexpress/hotplug.c
+++ arch/arm/mach-vexpress/hotplug.c
@@ -83,7 +83,7 @@ static inline void platform_do_lowpower(unsigned int cpu, int *spurious)
 	}
 }
 
-int platform_cpu_kill(unsigned int cpu)
+int vexpress_cpu_kill(unsigned int cpu)
 {
 	return 1;
 }
@@ -93,7 +93,7 @@ int platform_cpu_kill(unsigned int cpu)
  *
  * Called with IRQs disabled
  */
-void platform_cpu_die(unsigned int cpu)
+void vexpress_cpu_die(unsigned int cpu)
 {
 	int spurious = 0;
 
@@ -113,7 +113,7 @@ void platform_cpu_die(unsigned int cpu)
 		pr_warn("CPU%u: %u spurious wakeup calls\n", cpu, spurious);
 }
 
-int platform_cpu_disable(unsigned int cpu)
+int vexpress_cpu_disable(unsigned int cpu)
 {
 	/*
 	 * we don't allow CPU 0 to be shutdown (it is still too special
