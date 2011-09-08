4d85e3f43f28881a866e6ba089a2214cf09fe7f8
Marc Zyngier <marc.zyngier@arm.com>
2011-09-08 13:15:22 +0100

    ARM: SoC: convert Tegra to SoC descriptor
    Convert Tegra to use the SoC descriptor to provide its SMP
    and CPU hotplug operations.
    
    Tested on Harmony.
    
    Cc: Colin Cross <ccross@android.com>
    Acked-by: Stephen Warren <swarren@nvidia.com>
    Acked-by: Olof Johansson <olof@lixom.net>
    Signed-off-by: Marc Zyngier <marc.zyngier@arm.com>

--- arch/arm/mach-tegra/hotplug.c
+++ arch/arm/mach-tegra/hotplug.c
@@ -86,7 +86,7 @@ static inline void platform_do_lowpower(unsigned int cpu, int *spurious)
 	}
 }
 
-int platform_cpu_kill(unsigned int cpu)
+int tegra_cpu_kill(unsigned int cpu)
 {
 	return 1;
 }
@@ -96,7 +96,7 @@ int platform_cpu_kill(unsigned int cpu)
  *
  * Called with IRQs disabled
  */
-void platform_cpu_die(unsigned int cpu)
+void tegra_cpu_die(unsigned int cpu)
 {
 	int spurious = 0;
 
@@ -116,7 +116,7 @@ void platform_cpu_die(unsigned int cpu)
 		pr_warn("CPU%u: %u spurious wakeup calls\n", cpu, spurious);
 }
 
-int platform_cpu_disable(unsigned int cpu)
+int tegra_cpu_disable(unsigned int cpu)
 {
 	/*
 	 * we don't allow CPU 0 to be shutdown (it is still too special
