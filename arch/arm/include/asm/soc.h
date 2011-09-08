f05e0fb852b44d6bb7d5f9a47b21ab57e30ffac2
Marc Zyngier <marc.zyngier@arm.com>
2011-09-08 09:06:10 +0100

    ARM: SoC: Introduce per SoC descriptor
    The ARM core code expects the various SoCs to hide their
    implementation differences behind a well established API.
    The various sub-arch-specific bit are often either at
    the machine descriptor level, or provided at link time
    by the sub-arch code.
    
    The SoC descriptor is a container that holds the SoC
    specific bits that can be moved away from the machine
    descriptor as well as an indirection point for the
    global symbols (SMP and CPU hotplug support, for example).
    
    This patch introduce this SoC descriptor, with the only field
    being the name of the SoC.
    
    Cc: Arnd Bergmann <arnd@arndb.de>
    Cc: Nicolas Pitre <nico@fluxnic.net>
    Signed-off-by: Marc Zyngier <marc.zyngier@arm.com>

---
+++ arch/arm/include/asm/soc.h
@@ -0,0 +1,19 @@
+/*
+ *  linux/arch/arm/include/asm/soc.h
+ *
+ *  Copyright (C) 2011 ARM Ltd.
+ *  All Rights Reserved
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#ifndef __ASM_ARM_SOC_H
+#define __ASM_ARM_SOC_H
+
+struct arm_soc_desc {
+	const char			*name;
+};
+
+#endif	/* __ASM_ARM_SOC_H */
