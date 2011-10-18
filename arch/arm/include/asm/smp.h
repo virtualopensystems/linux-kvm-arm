a085f86a3bcd294b6e51bd5ee8101c022e2a1397
Dima Zavin <dima@android.com>
2011-10-18 16:59:54 -0700

    ARM: smp: implement arch_trigger_all_cpus_backtrace using IPI
    Based on a rough patch by frank.rowand@am.sony.com
    
    Since ARM doesn't have an NMI (fiq's are not always available),
    send an IPI to all other CPUs (current cpu prints the stack directly)
    to capture a backtrace.
    
    Change-Id: I8b163c8cec05d521b433ae133795865e8a33d4e2
    Signed-off-by: Dima Zavin <dima@android.com>

--- arch/arm/include/asm/smp.h
+++ arch/arm/include/asm/smp.h
@@ -80,6 +80,8 @@ extern void cpu_die(void);
 extern void arch_send_call_function_single_ipi(int cpu);
 extern void arch_send_call_function_ipi_mask(const struct cpumask *mask);
 
+extern void smp_send_all_cpu_backtrace(void);
+
 struct smp_operations {
 #ifdef CONFIG_SMP
 	/*
