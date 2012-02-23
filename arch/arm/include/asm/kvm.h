3ddeca4707a5e799934bf7210be26be2b31b5f57
Christoffer Dall <cdall@cs.columbia.edu>
2012-02-23 00:11:55 -0500

    ARM: KVM: Inject IRQs and FIQs from userspace
    Userspace can inject IRQs and FIQs through the KVM_IRQ_LINE VM ioctl.
    This ioctl is used since the sematics are in fact two lines that can be
    either raised or lowered on the VCPU - the IRQ and FIQ lines.
    
    KVM needs to know which VCPU it must operate on and whether the FIQ or
    IRQ line is raised/lowered. Hence both pieces of information is packed
    in the kvm_irq_level->irq field. The irq fild value will be:
      IRQ: vcpu_index << 1
      FIQ: (vcpu_index << 1) | 1
    
    This is documented in Documentation/kvm/api.txt.
    
    The effect of the ioctl is simply to simply raise/lower the
    corresponding irq_line field on the VCPU struct, which will cause the
    world-switch code to raise/lower virtual interrupts when running the
    guest on next switch. The wait_for_interrupt flag is also cleared for
    raised IRQs or FIQs causing an idle VCPU to become active again. CPUs
    in guest mode are kicked to make sure they refresh their interrupt status.
    
    Signed-off-by: Christoffer Dall <c.dall@virtualopensystems.com>

--- arch/arm/include/asm/kvm.h
+++ arch/arm/include/asm/kvm.h
@@ -20,6 +20,15 @@
 #include <asm/types.h>
 
 #define __KVM_HAVE_GUEST_DEBUG
+#define __KVM_HAVE_IRQ_LINE
+
+/*
+ * KVM_IRQ_LINE macros to set/read IRQ/FIQ for specific VCPU index.
+ */
+enum KVM_ARM_IRQ_LINE_TYPE {
+	KVM_ARM_IRQ_LINE = 0,
+	KVM_ARM_FIQ_LINE = 1,
+};
 
 /*
  * Modes used for short-hand mode determinition in the world-switch code and
