bae8795c155c2cdadb321c297a8851b58dd5820b
Sangwook Lee <sangwook.lee@linaro.org>
2012-05-08 11:45:25 +0100

    ARM: EXYNOS: Add G3D machine support for Origen board
    Signed-off-by: Sangwook Lee <sangwook.lee@linaro.org>

--- arch/arm/mach-exynos/mach-origen.c
+++ arch/arm/mach-exynos/mach-origen.c
@@ -866,6 +866,7 @@ static struct platform_device *origen_devices[] __initdata = {
 	&s5p_device_fimc_md,
 	&s5p_device_fimd0,
 	&s5p_device_g2d,
+	&s5p_device_g3d,
 	&s5p_device_hdmi,
 	&s5p_device_i2c_hdmiphy,
 	&s5p_device_jpeg,
