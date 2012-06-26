95a8fcfd0a98b1998d4aaec4eff5d9b2f24c2cc5
Christoffer Dall <c.dall@virtualopensystems.com>
2012-06-25 21:46:03 -0400

    ARM: KVM: Fix license headers

--- arch/arm/include/asm/kvm.h
+++ arch/arm/include/asm/kvm.h
@@ -1,4 +1,7 @@
 /*
+ * Copyright (C) 2012 - Virtual Open Systems and Columbia University
+ * Author: Christoffer Dall <c.dall@virtualopensystems.com>
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License, version 2, as
  * published by the Free Software Foundation.
@@ -11,7 +14,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- *
  */
 
 #ifndef __ARM_KVM_H__
--- arch/arm/kvm/exports.c
+++ arch/arm/kvm/exports.c
@@ -1,4 +1,7 @@
 /*
+ * Copyright (C) 2012 - Virtual Open Systems and Columbia University
+ * Author: Christoffer Dall <c.dall@virtualopensystems.com>
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License, version 2, as
  * published by the Free Software Foundation.
