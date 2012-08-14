adbffa11f55a52161e927d8b920dd7512e1aed84
Rusty Russell <rusty.russell@linaro.org>
2012-08-14 15:31:11 +0930

    tools/testing/selftests/kvm/arm: Use err() and errx()
    This is more convenient than perror and return.  We use exit status 2 if
    something went wrong with setting up the test and 1 if it failed.
    
    Signed-off-by: Rusty Russell <rusty.russell@linaro.org>

--- tools/testing/selftests/kvm/arm/guest-driver.h
+++ tools/testing/selftests/kvm/arm/guest-driver.h
@@ -1,12 +1,9 @@
 #ifndef __GUEST_DRIVER_H_
 #define __GUEST_DRIVER_H_
 
-#define pr_err(fmt, args...) fprintf(stderr, fmt, ##args)
-#define pr_errno(fmt, args...) \
-	fprintf(stderr, fmt ": %s\n", args, strerror(errno))
-
 #define PAGE_SIZE (4096)
 #define PAGE_MASK (~(PAGE_SIZE - 1))
+#define EXIT_SETUPFAIL 2
 
 #define CODE_SLOT 0
 #define CODE_PHYS_BASE (0x80000000)
