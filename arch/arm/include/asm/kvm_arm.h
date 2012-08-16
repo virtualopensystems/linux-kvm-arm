7c6247b703ea9c9ae5bd4d1ba76be68c5164b5e2
Christoffer Dall <c.dall@virtualopensystems.com>
2012-08-15 23:02:34 -0400

    KVM: ARM: Initial skeleton to compile KVM support
    Targets KVM support for Cortex A-15 processors.
    
    Contains all the framework components, make files, header files and some
    tracing functionality.
    
    Only supported core is Cortex-A15 for now.
    
    Contains minor reset hook driven from kvm_vcpu_set_target, which will
    eventually be a custom ARM ioctl to set the core we are emulating.
    
    Most functionality is in arch/arm/kvm/* or arch/arm/include/asm/kvm_*.h.
    
    “Nothing to see here. Move along, move along..."
    
    Signed-off-by: Rusty Russell <rusty.russell@linaro.org>
    Signed-off-by: Marc Zyngier <marc.zyngier@arm.com>
    Signed-off-by: Christoffer Dall <c.dall@virtualopensystems.com>

---
+++ arch/arm/include/asm/kvm_arm.h
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2012 - Virtual Open Systems and Columbia University
+ * Author: Christoffer Dall <c.dall@virtualopensystems.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 2, as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#ifndef __ARM_KVM_ARM_H__
+#define __ARM_KVM_ARM_H__
+
+/* Supported Processor Types */
+#define CORTEX_A15	(0xC0F)
+
+/* Multiprocessor Affinity Register */
+#define MPIDR_CPUID	(0x3 << 0)
+
+#endif /* __ARM_KVM_ARM_H__ */
