c7d0c0aee49d93033a23806827e8d1d54e020e0b
Rusty Russell <rusty.russell@linaro.org>
2012-09-11 15:40:20 +0930

    kvmtest: update for new API.
    Signed-off-by: Rusty Russell <rusty.russell@linaro.org>

--- tools/testing/selftests/kvm/arm/cp15_test.h
+++ tools/testing/selftests/kvm/arm/cp15_test.h
@@ -2,9 +2,9 @@
 #define CP15_TEST_H
 
 /* Make host set/get TTBR0 */
-#define CP15_TTBR0		(0xc0000000)
+#define CP15_TTBR0	(0xc0000000)
 
-/* Make host set/get TTBR0 and TTBR1, atomically. */
-#define CP15_TTBR0_TTBR1	(0xc0000004)
+/* Make host set/get IFAR. */
+#define CP15_IFAR	(0xc0000004)
 
 #endif /* CP15_TEST_H */
