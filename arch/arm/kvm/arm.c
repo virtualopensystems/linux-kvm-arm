6024647bad0e087a284c2335dd911cec15d56de9
Christoffer Dall <c.dall@virtualopensystems.com>
2012-09-13 21:34:13 -0400

    KVM: ARM: Compile after kvm/next merge
    kvm_arch_flush_shadow* have changed.
    
    Signed-off-by: Christoffer Dall <c.dall@virtualopensystems.com>

--- arch/arm/kvm/arm.c
+++ arch/arm/kvm/arm.c
@@ -244,7 +244,12 @@ void kvm_arch_commit_memory_region(struct kvm *kvm,
 {
 }
 
-void kvm_arch_flush_shadow(struct kvm *kvm)
+void kvm_arch_flush_shadow_all(struct kvm *kvm)
+{
+}
+
+void kvm_arch_flush_shadow_memslot(struct kvm *kvm,
+				   struct kvm_memory_slot *slot)
 {
 }
 
