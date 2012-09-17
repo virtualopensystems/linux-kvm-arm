f9fc459ead69c9ecf6bc6fc1c89c4982270df275
Antonios Motakis <a.motakis@virtualopensystems.com>
2012-09-17 08:47:45 +0200

    KVM: ARM: selftest: remove load multiple from MMIO test so it does not fail, since it is not currently planned to be implemented

--- tools/testing/selftests/kvm/arm/mmio-guest.c
+++ tools/testing/selftests/kvm/arm/mmio-guest.c
@@ -6,8 +6,6 @@ static char *io_data = IO_DATA;
 int test(void)
 {
 	register int r1 asm("r1");
-	register int r2 asm("r2");
-	register int r3 asm("r3");
 
 	print("Perform a simple load test\n");
 	asm volatile("ldr %0, [%1]" : "=r"(r1) : "r"(IO_DATA_BASE));
@@ -17,14 +15,6 @@ int test(void)
 	r1 = *(int *)IO_DATA;
 	asm volatile("str %0, [%1]" : : "r"(r1), "r"(IO_DATA_BASE));
 	ok();
-
-	print("Perform a load-multiple load test\n");
-	asm volatile("ldmia %3, { %0, %1, %2 }\n"
-		     : "=r"(r1), "=r"(r2), "=r"(r3)
-		     : "r"(IO_DATA_BASE));
-	assert(r1 == ((int *)IO_DATA)[0]
-	       && r2 == ((int *)IO_DATA)[1]
-	       && r2 == ((int *)IO_DATA)[2]);
 
 	return 0;
 }
