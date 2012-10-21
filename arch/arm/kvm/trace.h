03008fcca75a2586178dcd81085d5bfcffe4577e
Christoffer Dall <c.dall@virtualopensystems.com>
2012-10-21 15:03:24 -0400

    KVM: ARM: Emulation framework and CP15 emulation
    Adds a new important function in the main KVM/ARM code called
    handle_exit() which is called from kvm_arch_vcpu_ioctl_run() on returns
    from guest execution. This function examines the Hyp-Syndrome-Register
    (HSR), which contains information telling KVM what caused the exit from
    the guest.
    
    Some of the reasons for an exit are CP15 accesses, which are
    not allowed from the guest and this commit handles these exits by
    emulating the intended operation in software and skipping the guest
    instruction.
    
    Minor notes about the coproc register reset:
    1) We reserve a value of 0 as an invalid cp15 offset, to catch bugs in our
       table, at cost of 4 bytes per vcpu.
    
    2) Added comments on the table indicating how we handle each register, for
       simplicity of understanding.
    
    Reviewed-by: Marcelo Tosatti <mtosatti@redhat.com>
    Signed-off-by: Rusty Russell <rusty.russell@linaro.org>
    Signed-off-by: Christoffer Dall <c.dall@virtualopensystems.com>

--- arch/arm/kvm/trace.h
+++ arch/arm/kvm/trace.h
@@ -64,6 +64,51 @@ TRACE_EVENT(kvm_irq_line,
 		  __entry->type, __entry->vcpu_idx, __entry->irq_num, __entry->level)
 );
 
+/* Architecturally implementation defined CP15 register access */
+TRACE_EVENT(kvm_emulate_cp15_imp,
+	TP_PROTO(unsigned long Op1, unsigned long Rt1, unsigned long CRn,
+		 unsigned long CRm, unsigned long Op2, bool is_write),
+	TP_ARGS(Op1, Rt1, CRn, CRm, Op2, is_write),
+
+	TP_STRUCT__entry(
+		__field(	unsigned int,	Op1		)
+		__field(	unsigned int,	Rt1		)
+		__field(	unsigned int,	CRn		)
+		__field(	unsigned int,	CRm		)
+		__field(	unsigned int,	Op2		)
+		__field(	bool,		is_write	)
+	),
+
+	TP_fast_assign(
+		__entry->is_write		= is_write;
+		__entry->Op1			= Op1;
+		__entry->Rt1			= Rt1;
+		__entry->CRn			= CRn;
+		__entry->CRm			= CRm;
+		__entry->Op2			= Op2;
+	),
+
+	TP_printk("Implementation defined CP15: %s\tp15, %u, r%u, c%u, c%u, %u",
+			(__entry->is_write) ? "mcr" : "mrc",
+			__entry->Op1, __entry->Rt1, __entry->CRn,
+			__entry->CRm, __entry->Op2)
+);
+
+TRACE_EVENT(kvm_wfi,
+	TP_PROTO(unsigned long vcpu_pc),
+	TP_ARGS(vcpu_pc),
+
+	TP_STRUCT__entry(
+		__field(	unsigned long,	vcpu_pc		)
+	),
+
+	TP_fast_assign(
+		__entry->vcpu_pc		= vcpu_pc;
+	),
+
+	TP_printk("guest executed wfi at: 0x%08lx", __entry->vcpu_pc)
+);
+
 TRACE_EVENT(kvm_unmap_hva,
 	TP_PROTO(unsigned long hva),
 	TP_ARGS(hva),
