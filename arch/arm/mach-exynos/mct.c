65c2772a0ab5f2834b10498b7c7ba7e9d4885f8e
Christoffer Dall <c.dall@virtualopensystems.com>
2012-11-18 23:54:05 -0500

    ARM: SoC: Add arch timer to exynos5250
    Add the arch timer node in the DT and initialize and register the arch
    timers in the main exynos timer code.
    
    Signed-off-by: Christoffer Dall <c.dall@virtualopensystems.com>

--- arch/arm/mach-exynos/mct.c
+++ arch/arm/mach-exynos/mct.c
@@ -24,6 +24,7 @@
 #include <asm/arch_timer.h>
 #include <asm/hardware/gic.h>
 #include <asm/localtimer.h>
+#include <asm/arch_timer.h>
 
 #include <plat/cpu.h>
 
@@ -493,6 +494,9 @@ static void __init exynos_timer_init(void)
 	exynos4_timer_resources();
 	exynos4_clocksource_init();
 	exynos4_clockevent_init();
+
+	if (arch_timer_of_register() != 0)
+		pr_err("Error registering architected timers\n");
 }
 
 struct sys_timer exynos4_timer = {
