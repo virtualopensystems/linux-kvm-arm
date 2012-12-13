eba5631c851697a5dcbf155c09b96d0bff43e17b
John Stultz <john.stultz@linaro.org>
2012-12-13 15:41:01 -0500

    ion: Compile fixup, as __dma_page_cpu_to_dev() is now static
    Use dma_sync_single_for_device() rather then __dma_page_cpu_to_dev()
    
    Signed-off-by: John Stultz <john.stultz@linaro.org>

--- drivers/gpu/ion/ion_system_heap.c
+++ drivers/gpu/ion/ion_system_heap.c
@@ -78,8 +78,10 @@ static struct page *alloc_buffer_page(struct ion_system_heap *heap,
 		page = alloc_pages(gfp_flags, order);
 		if (!page)
 			return 0;
-		__dma_page_cpu_to_dev(page, 0, PAGE_SIZE << order,
-				      DMA_BIDIRECTIONAL);
+		dma_sync_single_for_device(NULL,
+					pfn_to_dma(NULL, page_to_pfn(page)),
+					PAGE_SIZE << order,
+					DMA_BIDIRECTIONAL);
 	}
 	if (!page)
 		return 0;
