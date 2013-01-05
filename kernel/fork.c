47e331a4fbbb7b7da33c112a72bbbb3259d8402d
Alan Cox <alan@linux.intel.com>
2013-01-05 12:15:29 +1100

    fork: unshare: remove dead code
    If new_nsproxy is set we will always call switch_task_namespaces and then
    set new_nsproxy back to NULL so the reassignment and fall through check
    are redundant
    
    Signed-off-by: Alan Cox <alan@linux.intel.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- kernel/fork.c
+++ kernel/fork.c
@@ -1853,10 +1853,8 @@ SYSCALL_DEFINE1(unshare, unsigned long, unshare_flags)
 			exit_sem(current);
 		}
 
-		if (new_nsproxy) {
+		if (new_nsproxy)
 			switch_task_namespaces(current, new_nsproxy);
-			new_nsproxy = NULL;
-		}
 
 		task_lock(current);
 
@@ -1885,9 +1883,6 @@ SYSCALL_DEFINE1(unshare, unsigned long, unshare_flags)
 			new_cred = NULL;
 		}
 	}
-
-	if (new_nsproxy)
-		put_nsproxy(new_nsproxy);
 
 bad_unshare_cleanup_cred:
 	if (new_cred)
