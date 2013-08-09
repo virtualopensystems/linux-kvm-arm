11d75fb158817d87bc64bc8d1079587665dbc91d
Nikolay Nikolaev <n.nikolaev@virtualopensystems.com>
2013-08-09 10:17:41 +0300

    MAX77686: add support for EN32KHZ_AP, EN32KHZ_CP and P32KHZ
    EN32KHZ_AP, EN32KHZ_CP, P32KHZ : fake regulators which actually produce 32KHz clocks
    
    Signed-off-by: Nikolay Nikolaev <n.nikolaev@virtualopensystems.com>

--- drivers/regulator/max77686.c
+++ drivers/regulator/max77686.c
@@ -57,6 +57,11 @@
 #define MAX77686_REGULATORS	MAX77686_REG_MAX
 #define MAX77686_LDOS		26
 
+#define MAX77686_CLOCK_OPMODE_MASK	0x1
+#define MAX77686_CLOCK_EN32KHZ_AP_SHIFT	0x0
+#define MAX77686_CLOCK_EN32KHZ_CP_SHIFT	0x1
+#define MAX77686_CLOCK_P32KHZ_SHIFT	0x2
+
 enum max77686_ramp_rate {
 	RAMP_RATE_13P75MV,
 	RAMP_RATE_27P5MV,
@@ -240,6 +245,12 @@ static struct regulator_ops max77686_buck_dvs_ops = {
 	.set_suspend_disable	= max77686_buck_set_suspend_disable,
 };
 
+static struct regulator_ops max77686_fixedvolt_ops = {
+	.is_enabled		= regulator_is_enabled_regmap,
+	.enable			= max77686_enable,
+	.disable		= regulator_disable_regmap,
+};
+
 #define regulator_desc_ldo(num)		{				\
 	.name		= "LDO"#num,					\
 	.id		= MAX77686_LDO##num,				\
@@ -387,6 +398,34 @@ static struct regulator_desc regulators[] = {
 	regulator_desc_buck(7),
 	regulator_desc_buck(8),
 	regulator_desc_buck(9),
+	{
+		.name = "EN32KHZ_AP",
+		.id = MAX77686_EN32KHZ_AP,
+		.ops = &max77686_fixedvolt_ops,
+		.type = REGULATOR_VOLTAGE,
+		.owner = THIS_MODULE,
+		.enable_reg = MAX77686_REG_32KHZ,
+		.enable_mask = MAX77686_CLOCK_OPMODE_MASK	\
+			       << MAX77686_CLOCK_EN32KHZ_AP_SHIFT,
+	}, {
+		.name = "EN32KHZ_CP",
+		.id = MAX77686_EN32KHZ_CP,
+		.ops = &max77686_fixedvolt_ops,
+		.type = REGULATOR_VOLTAGE,
+		.owner = THIS_MODULE,
+		.enable_reg = MAX77686_REG_32KHZ,
+		.enable_mask = MAX77686_CLOCK_OPMODE_MASK	\
+			       << MAX77686_CLOCK_EN32KHZ_CP_SHIFT,
+	}, {
+		.name = "P32KHZ",
+		.id = MAX77686_P32KHZ,
+		.ops = &max77686_fixedvolt_ops,
+		.type = REGULATOR_VOLTAGE,
+		.owner = THIS_MODULE,
+		.enable_reg = MAX77686_REG_32KHZ,
+		.enable_mask = MAX77686_CLOCK_OPMODE_MASK	\
+			       << MAX77686_CLOCK_P32KHZ_SHIFT,
+	},
 };
 
 #ifdef CONFIG_OF
