5d8e593caf982f7ee29c04e1a494a84749c42896
Antonios Motakis <a.motakis@virtualopensystems.com>
2014-06-04 00:56:18 +0200

    iommu/arm-smmu: add IOMMU_CAP_NOEXEC to the ARM SMMU driver
    The ARM SMMU supports the IOMMU_NOEXEC protection flag. Add the
    corresponding IOMMU capability.
    
    Signed-off-by: Antonios Motakis <a.motakis@virtualopensystems.com>

--- drivers/iommu/arm-smmu.c
+++ drivers/iommu/arm-smmu.c
@@ -1544,6 +1544,8 @@ static int arm_smmu_domain_has_cap(struct iommu_domain *domain,
 	if (smmu_domain->root_cfg.smmu->features & ARM_SMMU_FEAT_COHERENT_WALK)
 		caps |= IOMMU_CAP_CACHE_COHERENCY;
 
+	caps |= IOMMU_CAP_NOEXEC;
+
 	return !!(cap & caps);
 }
 
