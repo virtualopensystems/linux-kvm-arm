547efb22994402f8d66e597d212d4ca3cbe0a392
Antonios Motakis <a.motakis@virtualopensystems.com>
2014-10-13 13:32:43 +0200

    vfio: pass an opaque pointer on virqfd initialization
    VFIO_PCI passes the VFIO device structure *vdev via eventfd to the handler
    that implements masking/unmasking of IRQs via an eventfd. We can replace
    it in the virqfd infrastructure with an opaque type so we can make use
    of the mechanism from other VFIO bus drivers.
    
    Signed-off-by: Antonios Motakis <a.motakis@virtualopensystems.com>

--- include/linux/vfio.h
+++ include/linux/vfio.h
@@ -128,10 +128,10 @@ static inline long vfio_spapr_iommu_eeh_ioctl(struct iommu_group *group,
  * IRQFD support
  */
 struct virqfd {
-	struct vfio_pci_device	*vdev;
+	void			*opaque;
 	struct eventfd_ctx	*eventfd;
-	int			(*handler)(struct vfio_pci_device *, void *);
-	void			(*thread)(struct vfio_pci_device *, void *);
+	int			(*handler)(void *, void *);
+	void			(*thread)(void *, void *);
 	void			*data;
 	struct work_struct	inject;
 	wait_queue_t		wait;
@@ -142,9 +142,9 @@ struct virqfd {
 
 extern int vfio_pci_virqfd_init(void);
 extern void vfio_pci_virqfd_exit(void);
-extern int virqfd_enable(struct vfio_pci_device *vdev,
-			 int (*handler)(struct vfio_pci_device *, void *),
-			 void (*thread)(struct vfio_pci_device *, void *),
+extern int virqfd_enable(void *opaque,
+			 int (*handler)(void *, void *),
+			 void (*thread)(void *, void *),
 			 void *data, struct virqfd **pvirqfd, int fd);
 extern void virqfd_disable(struct virqfd **pvirqfd);
 
