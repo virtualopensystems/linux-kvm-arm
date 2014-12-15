6a8f3425bfd2fa86461379ba95df183dfe73567d
Antonios Motakis <a.motakis@virtualopensystems.com>
2014-12-15 17:19:25 +0100

    vfio: introduce the VFIO_DMA_MAP_FLAG_NOEXEC flag
    We introduce the VFIO_DMA_MAP_FLAG_NOEXEC flag to the VFIO dma map call,
    and expose its availability via the capability VFIO_DMA_NOEXEC_IOMMU.
    This way the user can control whether the XN flag will be set on the
    requested mappings. The IOMMU_NOEXEC flag needs to be available for all
    the IOMMUs of the container used.
    
    Signed-off-by: Antonios Motakis <a.motakis@virtualopensystems.com>

--- include/uapi/linux/vfio.h
+++ include/uapi/linux/vfio.h
@@ -31,6 +31,7 @@ enum vfio_iommu_cap {
 					   (ex. PCIe NoSnoop stripping) */
 	VFIO_EEH = 5,			/* Check if EEH is supported */
 	VFIO_TYPE1_NESTING_IOMMU = 6,	/* Two-stage IOMMU, implies v2  */
+	VFIO_DMA_NOEXEC_IOMMU = 7,
 };
 
 
@@ -396,12 +397,17 @@ struct vfio_iommu_type1_info {
  *
  * Map process virtual addresses to IO virtual addresses using the
  * provided struct vfio_dma_map. Caller sets argsz. READ &/ WRITE required.
+ *
+ * To use the VFIO_DMA_MAP_FLAG_NOEXEC flag, the container must support the
+ * VFIO_DMA_NOEXEC_IOMMU capability. If mappings are created using this flag,
+ * any groups subsequently added to the container must support this capability.
  */
 struct vfio_iommu_type1_dma_map {
 	__u32	argsz;
 	__u32	flags;
 #define VFIO_DMA_MAP_FLAG_READ (1 << 0)		/* readable from device */
 #define VFIO_DMA_MAP_FLAG_WRITE (1 << 1)	/* writable from device */
+#define VFIO_DMA_MAP_FLAG_NOEXEC (1 << 2)	/* not executable from device */
 	__u64	vaddr;				/* Process virtual address */
 	__u64	iova;				/* IO virtual address */
 	__u64	size;				/* Size of mapping (bytes) */
